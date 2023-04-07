170a0c20ec97116e2531f94d537442d94e4019aa
HarisM <harismahjoob42@gmail.com>
2023-04-06 17:58:52 -0700

    typo

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const sequelize = require('./config/connection');
 const path = require('path');
 
 const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
