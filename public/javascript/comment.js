9f89685ac9b616026f2f980695596efbb2423338
HarisM <harismahjoob42@gmail.com>
2023-04-07 13:42:34 -0700

    comments

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,42 +1,30 @@
-const commentSubmitHandler = async (event) => {
+async function commentFormHandler(event) {
     event.preventDefault();
-
-    const text = document.querySelector('#comment-input').value.trim();
-    const post_id = document.querySelector('#post-id').value.trim();
-    const user_id = document.querySelector('#user-id').value.trim();
-
-    if (text) {
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
         const response = await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                text,
-                user_id,
-                post_id
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_text
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
         });
-
+      
         if (response.ok) {
-            document.location.reload();
+          document.location.reload();
         } else {
-            alert(response.statusText);
+          alert(response.statusText);
         }
-    }
-}
-
-const deleteComment = async (event) => {
-     if (event.value) {
-        const response = await fetch(`/api/comment/${event.value}`, {
-            method: 'DELETE',
-        });
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
-     }
-}
-
-document.querySelector('#comment-submit').addEventListener('click', commentSubmitHandler);
+      }
+  }
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
