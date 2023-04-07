44394e4f4aa2b1f560e536ad9ab300bf8b7db348
HarisM <harismahjoob42@gmail.com>
2023-04-06 20:03:54 -0700

    added dialect

--- config/connection.js
+++ config/connection.js
@@ -12,9 +12,12 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME, 
         process.env.DB_USER, 
-        process.env.DB_PW, 
-        
-    );
+        process.env.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+    });    
+    
 }
 
 module.exports = sequelize;
