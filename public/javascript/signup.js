e722ad44e69c6068cb8e277a513981b57d9554f9
HarisM <harismahjoob42@gmail.com>
2023-04-06 18:11:47 -0700

    typo

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -5,7 +5,7 @@ async function signupFormHandler(event) {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     const twitter = document.querySelector('#twitter-signup').value.trim();
-    const github = document.querySelector('#github-signup').value.trime();
+    const github = document.querySelector('#github-signup').value.trim();
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
