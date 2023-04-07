d6c7f15451074ed515fa838e002b64b9005e0a2c
HarisM <harismahjoob42@gmail.com>
2023-04-06 17:23:11 -0700

    finished routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // User id from session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'twitter', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'twitter', 'github']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // Serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true}));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/create/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // Use id from session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'twitter', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'twitter', 'github']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            //Serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('create-post', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
