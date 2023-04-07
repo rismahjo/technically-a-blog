1fcf1ae4828aafead78d142539bf415487f885ac
HarisM <harismahjoob42@gmail.com>
2023-04-06 20:27:39 -0700

    fix

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,8 @@ if (process.env.JAWSDB_URL) {
         process.env.DB_USER, 
         process.env.DB_PW, {
             host: 'localhost',
-            dialect: 'mysql'
+            dialect: 'mysql',
+            port: 3306
     });    
     
 }
